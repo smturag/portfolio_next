eae4d961cf1bec2940d4c68ce0b52e88461034e2
smturag <smturag@pop-os.localdomain>
2023-04-26 00:54:08 +0600

    done

--- src/pages/Component/AboutDetails.jsx
+++ src/pages/Component/AboutDetails.jsx
@@ -4,8 +4,6 @@ import ShowCmp from "./ShowCmp";
 import { Box, Grid } from "@mui/material";
 
 export default function AboutDetails() {
-
-    
   return (
     <Box className={styles.info_container}>
       <Grid container direction={"row"}>
@@ -13,13 +11,12 @@ export default function AboutDetails() {
           <Box sx={{ padding: "25px" }}>
             <h2 className="details">
               I'm Simone Olivia, a Web Developer I help you build brand for your
-              business at an affordable price. Thousands of clients have
-              procured exceptional results while working with our dedicated
-              team. when an unknown printer took a galley of type and scrambled
-              it to make a type specimen book. Delivering work within time and
-              budget which meets client’s requirements is our moto. Lorem Ipsum
-              has been the industry's standard dummy text ever when an unknown
-              printer took a galley.
+              business at an affordable price. Full Stack Web Developer
+              specializing in front end & backend development. Experienced with
+              all stages of the development cycle for dynamic web projects.
+              Well-versed in numerous programming languages including HTML5, PHP
+              OOP, JavaScript, CSS, MySQL. Strong background in project
+              management and customer relations.
             </h2>
             <style jsx>
               {`
